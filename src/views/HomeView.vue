1de03c245f595dd95f90223bb007d74b7fe594e2
GONTARDLukas <lukas.gontard@etu.univ-smb.fr>
2025-03-13 11:49:00 +0100

    début de drop-down

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,84 @@
-<script setup lang="ts"></script>
+
+<script setup lang="ts">
+	import '@/assets/sass/home.scss';
+
+</script>
+
 
 <template>
+
 	<main class="home">
-		<h1>Miliboo</h1>
+		<nav id="productCategory">
+			<span>
+				<label for="button-menu-1">Canapé & Fauteuil</label>
+				<input type="checkbox" class="drop-down" id="button-menu-1"></input>
+			</span>
+			<div class="menu" id="menu-1"></div>
+
+			<span>
+				<label for="button-menu-2">Chaise & Tabouret</label>
+				<input type="checkbox" class="drop-down" id="button-menu-2"></input>
+			</span>
+			<div class="menu" id="menu-2"></div>
+
+			<span>
+				<label for="button-menu-3">Bureau</label>
+				<input type="checkbox" class="drop-down" id="button-menu-3"></input>
+			</span>
+			<div class="menu" id="menu-3"></div>
+			
+			<span>
+				<label for="button-menu-4">Table</label>
+				<input type="checkbox" class="drop-down" id="button-menu-4"></input>
+			</span>
+			<div class="menu" id="menu-4"></div>
+
+			<span>
+				<label for="button-menu-5">Rangement</label>
+				<input type="checkbox" class="drop-down" id="button-menu-5"></input>
+			</span>
+			<div class="menu" id="menu-5"></div>
+
+			<span>
+				<label for="button-menu-6">Chambre</label>
+				<input type="checkbox" class="drop-down" id="button-menu-6"></input>
+			</span>
+			<div class="menu" id="menu-6"></div>
+
+			<span>
+				<label for="button-menu-7">Enfant</label>
+				<input type="checkbox" class="drop-down" id="button-menu-7"></input>
+			</span>
+			<div class="menu" id="menu-7"></div>
+
+			<span>
+				<label for="button-menu-8">Jardin</label>
+				<input type="checkbox" class="drop-down" id="button-menu-8"></input>
+			</span>
+			<div class="menu" id="menu-8"></div>
+
+			<span>
+				<label for="button-menu-9">Luminaire</label>
+				<input type="checkbox" class="drop-down" id="button-menu-9"></input>
+			</span>
+			<div class="menu" id="menu-9"></div>
+
+			<span>
+				<label for="button-menu-10">Déco</label>			
+				<input type="checkbox" class="drop-down" id="button-menu-10"></input>
+			</span>
+			<div class="menu" id="menu-10"></div>
+			
+			<span>
+				<label for="button-menu-11">Meubles reconditionnés</label>
+				<input type="checkbox" class="drop-down" id="button-menu-11"></input>
+
+			</span>
+			<div class="menu" id="menu-11"></div>
+
+		</nav>	
+		<video src=".src/assets/imageshome_mars_2025.mp4"></video>
 	</main>
+	
 </template>
+<style scoped></style>
