abe8a2914185676783b728f22bb903e61a34429f
kaplego <70806521+kaplego@users.noreply.github.com>
2025-03-13 13:36:54 +0100

    RÉPARÉ ERREURS DE LUKAS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,84 +1,77 @@
-
 <script setup lang="ts">
-	import '@/assets/sass/home.scss';
-
+import '@/assets/sass/home.scss';
 </script>
 
-
 <template>
-
 	<main class="home">
 		<nav id="productCategory">
 			<span>
 				<label for="button-menu-1">Canapé & Fauteuil</label>
-				<input type="checkbox" class="drop-down" id="button-menu-1"></input>
+				<input type="checkbox" class="drop-down" id="button-menu-1" />
 			</span>
 			<div class="menu" id="menu-1"></div>
 
 			<span>
 				<label for="button-menu-2">Chaise & Tabouret</label>
-				<input type="checkbox" class="drop-down" id="button-menu-2"></input>
+				<input type="checkbox" class="drop-down" id="button-menu-2" />
 			</span>
 			<div class="menu" id="menu-2"></div>
 
 			<span>
 				<label for="button-menu-3">Bureau</label>
-				<input type="checkbox" class="drop-down" id="button-menu-3"></input>
+				<input type="checkbox" class="drop-down" id="button-menu-3" />
 			</span>
 			<div class="menu" id="menu-3"></div>
-			
+
 			<span>
 				<label for="button-menu-4">Table</label>
-				<input type="checkbox" class="drop-down" id="button-menu-4"></input>
+				<input type="checkbox" class="drop-down" id="button-menu-4" />
 			</span>
 			<div class="menu" id="menu-4"></div>
 
 			<span>
 				<label for="button-menu-5">Rangement</label>
-				<input type="checkbox" class="drop-down" id="button-menu-5"></input>
+				<input type="checkbox" class="drop-down" id="button-menu-5" />
 			</span>
 			<div class="menu" id="menu-5"></div>
 
 			<span>
 				<label for="button-menu-6">Chambre</label>
-				<input type="checkbox" class="drop-down" id="button-menu-6"></input>
+				<input type="checkbox" class="drop-down" id="button-menu-6" />
 			</span>
 			<div class="menu" id="menu-6"></div>
 
 			<span>
 				<label for="button-menu-7">Enfant</label>
-				<input type="checkbox" class="drop-down" id="button-menu-7"></input>
+				<input type="checkbox" class="drop-down" id="button-menu-7" />
 			</span>
 			<div class="menu" id="menu-7"></div>
 
 			<span>
 				<label for="button-menu-8">Jardin</label>
-				<input type="checkbox" class="drop-down" id="button-menu-8"></input>
+				<input type="checkbox" class="drop-down" id="button-menu-8" />
 			</span>
 			<div class="menu" id="menu-8"></div>
 
 			<span>
 				<label for="button-menu-9">Luminaire</label>
-				<input type="checkbox" class="drop-down" id="button-menu-9"></input>
+				<input type="checkbox" class="drop-down" id="button-menu-9" />
 			</span>
 			<div class="menu" id="menu-9"></div>
 
 			<span>
-				<label for="button-menu-10">Déco</label>			
-				<input type="checkbox" class="drop-down" id="button-menu-10"></input>
+				<label for="button-menu-10">Déco</label>
+				<input type="checkbox" class="drop-down" id="button-menu-10" />
 			</span>
 			<div class="menu" id="menu-10"></div>
-			
+
 			<span>
 				<label for="button-menu-11">Meubles reconditionnés</label>
-				<input type="checkbox" class="drop-down" id="button-menu-11"></input>
-
+				<input type="checkbox" class="drop-down" id="button-menu-11" />
 			</span>
 			<div class="menu" id="menu-11"></div>
-
-		</nav>	
+		</nav>
 		<video src=".src/assets/imageshome_mars_2025.mp4"></video>
 	</main>
-	
 </template>
 <style scoped></style>
