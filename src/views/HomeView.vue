0a8beae1a70715746ec3eb245750b5a65a9b5121
GONTARDLukas <lukas.gontard@etu.univ-smb.fr>
2025-03-14 09:52:29 +0100

    Ajout vidéo + corrections drop-down

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,76 +1,73 @@
 <script setup lang="ts">
 import '@/assets/sass/home.scss';
+
 </script>
+
 
 <template>
 	<main class="home">
 		<nav id="productCategory">
-			<span>
-				<label for="button-menu-1">Canapé & Fauteuil</label>
-				<input type="checkbox" class="drop-down" id="button-menu-1" />
-			</span>
+			<label for="button-menu-1">Canapé & Fauteuil</label>
+			<input type="checkbox" class="drop-down" id="button-menu-1" />
+
+
+			<label for="button-menu-2">Chaise & Tabouret</label>
+			<input type="checkbox" class="drop-down" id="button-menu-2" />
+
+			<label for="button-menu-3">Bureau</label>
+			<input type="checkbox" class="drop-down" id="button-menu-3" />
+
+			<label for="button-menu-4">Table</label>
+			<input type="checkbox" class="drop-down" id="button-menu-4" />
+
+			<label for="button-menu-5">Rangement</label>
+			<input type="checkbox" class="drop-down" id="button-menu-5" />
+
+			<label for="button-menu-6">Chambre</label>
+			<input type="checkbox" class="drop-down" id="button-menu-6" />
+
+			<label for="button-menu-7">Enfant</label>
+			<input type="checkbox" class="drop-down" id="button-menu-7" />
+
+			<label for="button-menu-8">Jardin</label>
+			<input type="checkbox" class="drop-down" id="button-menu-8" />
+
+			<label for="button-menu-9">Luminaire</label>
+			<input type="checkbox" class="drop-down" id="button-menu-9" />
+
+			<label for="button-menu-10">Déco</label>
+			<input type="checkbox" class="drop-down" id="button-menu-10" />
+
+			<label for="button-menu-11">Meubles reconditionnés</label>
+			<input type="checkbox" class="drop-down" id="button-menu-11" />
 			<div class="menu" id="menu-1"></div>
 
-			<span>
-				<label for="button-menu-2">Chaise & Tabouret</label>
-				<input type="checkbox" class="drop-down" id="button-menu-2" />
-			</span>
+		</nav>
+		<div class="content">
+
 			<div class="menu" id="menu-2"></div>
 
-			<span>
-				<label for="button-menu-3">Bureau</label>
-				<input type="checkbox" class="drop-down" id="button-menu-3" />
-			</span>
 			<div class="menu" id="menu-3"></div>
 
-			<span>
-				<label for="button-menu-4">Table</label>
-				<input type="checkbox" class="drop-down" id="button-menu-4" />
-			</span>
 			<div class="menu" id="menu-4"></div>
 
-			<span>
-				<label for="button-menu-5">Rangement</label>
-				<input type="checkbox" class="drop-down" id="button-menu-5" />
-			</span>
 			<div class="menu" id="menu-5"></div>
 
-			<span>
-				<label for="button-menu-6">Chambre</label>
-				<input type="checkbox" class="drop-down" id="button-menu-6" />
-			</span>
 			<div class="menu" id="menu-6"></div>
 
-			<span>
-				<label for="button-menu-7">Enfant</label>
-				<input type="checkbox" class="drop-down" id="button-menu-7" />
-			</span>
 			<div class="menu" id="menu-7"></div>
 
-			<span>
-				<label for="button-menu-8">Jardin</label>
-				<input type="checkbox" class="drop-down" id="button-menu-8" />
-			</span>
 			<div class="menu" id="menu-8"></div>
 
-			<span>
-				<label for="button-menu-9">Luminaire</label>
-				<input type="checkbox" class="drop-down" id="button-menu-9" />
-			</span>
 			<div class="menu" id="menu-9"></div>
 
-			<span>
-				<label for="button-menu-10">Déco</label>
-				<input type="checkbox" class="drop-down" id="button-menu-10" />
-			</span>
 			<div class="menu" id="menu-10"></div>
 
-			<span>
-				<label for="button-menu-11">Meubles reconditionnés</label>
-				<input type="checkbox" class="drop-down" id="button-menu-11" />
-			</span>
 			<div class="menu" id="menu-11"></div>
-		</nav>
+		</div>
+		<video id="animatedBackground" ref="video" autoplay preload="auto" >
+			<source  src='@/assets/images/home_mars_2025.mp4' type="video/mp4" />
+		</video>
 	</main>
 </template>
 <style scoped></style>
