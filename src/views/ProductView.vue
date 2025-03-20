8b83628953467da4d6016ba79c65081eed4431da
GONTARDLukas <lukas.gontard@etu.univ-smb.fr>
2025-03-20 11:36:47 +0100

    Ajout de notation + début css page produit

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -6,14 +6,48 @@ const isLoading = useLoadingStore();
 isLoading.switchLoading(false);
 const products = useProductsStore();
 const product = products.list?.find((x:Produit) => x.idproduit.toString()==useRouter().currentRoute.value.params.id);
-console.log(product);
+
 </script>
 
 <template>
 	<main>
 		<div class="container">
-			<h1>{{product?.nomproduit}}</h1>
+			<div class="illustrations-card">
+				<img src="https://placehold.co/800x800/PNG" :alt="product.nomproduit" class="photo" />
+				<img src="https://placehold.co/800x800/PNG" :alt="product.nomproduit" class="photo" />
+				<img src="https://placehold.co/800x800/PNG" :alt="product.nomproduit" class="photo" />
+				<img src="https://placehold.co/800x800/PNG" :alt="product.nomproduit" class="photo" />
+
+				<div class="information-card">
+					<h1>{{product?.nomproduit}}</h1>
+					<a href="#description">Description détaillée</a>
+					<div class="star-container">
+						<span class="fa fa-star checked">★</span>
+						<span class="fa fa-star checked">★</span>
+						<span class="fa fa-star checked">★</span>
+						<span class="fa fa-star">★</span>
+						<span class="fa fa-star">★</span>
+						<p>({{product?.avisNavigation.length}} avis)</p>
+					</div>
+
+				</div>
+			</div>
+			<div id="description">
+				<p></p>
+			</div>
+
             <p>{{product}}</p>
 		</div>
 	</main>
 </template>
+<style lang="scss">
+	.star-container{
+		padding:0;
+		display:flex;
+		align-item:center;
+
+	}
+	.fa.fa-star{
+		font-size:15px;
+	}
+</style>
