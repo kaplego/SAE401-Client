def8dd4dcbdf4d2c74f994cf4293b637f9160388
kaplego <70806521+kaplego@users.noreply.github.com>
2025-03-20 10:29:42 +0100

    CSS Produit

--- src/components/CardProduit.vue
+++ src/components/CardProduit.vue
@@ -13,11 +13,13 @@ const colorationLaMoinsChere = props.produit.colorationsNavigation.reduce(
 	props.produit.colorationsNavigation[0],
 );
 
-props.produit.colorationsNavigation.sort((a, b) => (a == colorationLaMoinsChere ? -1 : b == colorationLaMoinsChere ? 1 : 0));
+props.produit.colorationsNavigation.sort((a, b) =>
+	a == colorationLaMoinsChere ? -1 : b == colorationLaMoinsChere ? 1 : 0,
+);
 </script>
 
 <template>
-	<RouterLink :to="'/produits/'+props.produit.idproduit">
+	<RouterLink :to="'/produits/' + props.produit.idproduit">
 		<div class="card-produit">
 			<div class="banniere-soldes-container">
 				<div class="banniere-soldes" v-if="colorationLaMoinsChere?.prixsolde !== null">Soldes</div>
@@ -47,3 +49,101 @@ props.produit.colorationsNavigation.sort((a, b) => (a == colorationLaMoinsChere 
 		</div>
 	</RouterLink>
 </template>
+
+<style lang="scss">
+.card-produit {
+	--ratio: 1.5;
+	aspect-ratio: 1 / var(--ratio);
+	background-color: var(--t-background2);
+	border-radius: 8px;
+	width: 100%;
+	border: 2px solid var(--t-background1);
+	cursor: pointer;
+	transition: all 100ms;
+	position: relative;
+
+	display: grid;
+	grid-template-rows: calc(100% / var(--ratio)) auto;
+
+	&:hover {
+		background-color: var(--t-background1-accent);
+		border-color: var(--t-border1-accent);
+	}
+
+	.banniere-soldes-container {
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		top: 0;
+		left: 0;
+		overflow: hidden;
+
+		.banniere-soldes {
+			position: absolute;
+			width: 100%;
+			background-color: var(--c-red-500);
+			color: var(--c-gray-50);
+			transform: rotateZ(45deg) translateY(50px);
+			transform-origin: center center;
+			right: -50%;
+			text-align: center;
+			z-index: 50;
+			text-transform: uppercase;
+			font-weight: bold;
+			letter-spacing: 2px;
+			user-select: none;
+		}
+	}
+
+	.photo {
+		aspect-ratio: 1;
+		width: 100%;
+		border-radius: 6px 6px 0 0;
+	}
+
+	.content {
+		padding: 0.5rem 1rem;
+		height: 100%;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		gap: 0.5rem;
+
+		.nom {
+			font-family: 'Rubik';
+			overflow: hidden;
+		}
+
+		.prix {
+			margin-bottom: 0.5rem;
+
+			.prixsolde {
+				color: var(--c-red-500);
+				font-weight: bold;
+				margin-right: 6px;
+			}
+
+			.prixvente-solde {
+				text-decoration: line-through;
+				font-weight: normal;
+				color: var(--t-foreground3);
+			}
+		}
+
+		.colorations {
+			display: flex;
+			height: 20px;
+			gap: 0.5rem;
+
+			.coloration {
+				aspect-ratio: 1;
+				height: 100%;
+				border: 1px solid var(--t-foreground1);
+				background-color: var(--couleur);
+				border-radius: 100vw;
+			}
+		}
+	}
+}
+</style>
