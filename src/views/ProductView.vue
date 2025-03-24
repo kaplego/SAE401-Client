48306e4c9e36f94d28ab09d0e1b425cb72f72d75
GONTARDLukas <lukas.gontard@etu.univ-smb.fr>
2025-03-24 16:40:00 +0000

    amélioration css, création des boutons d'achat et de la modulation de la quantité des items

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -23,41 +23,50 @@ watchEffect(() => {
 	<main v-if="product">
 		<div class="container">
 			<div class="illustrations-card">
-				<img src="https://placehold.co/800x800/PNG" :alt="product.nomproduit" class="photo" />
-				<img src="https://placehold.co/800x800/PNG" :alt="product.nomproduit" class="photo" />
-				<img src="https://placehold.co/800x800/PNG" :alt="product.nomproduit" class="photo" />
-				<img src="https://placehold.co/800x800/PNG" :alt="product.nomproduit" class="photo" />
-
+				<div class="images-card">
+					<img src="https://placehold.co/800x800/PNG" :alt="product.nomproduit" class="photo" />
+					<img src="https://placehold.co/800x800/PNG" :alt="product.nomproduit" class="photo" />
+					<img src="https://placehold.co/800x800/PNG" :alt="product.nomproduit" class="photo" />
+					<img src="https://placehold.co/800x800/PNG" :alt="product.nomproduit" class="photo" />
+				</div>
 				<div class="information-card">
-					<h1>{{ product?.nomproduit }}</h1>
-					<a href="#description">Description détaillée</a>
-					<div class="star-container">
-						<Star />
-						<Star />
-						<Star />
-						<Star />
-						<Star />
-
-						<p>({{ product.avisNavigation.length }} avis)</p>
+					<div class="scroll-element">
+						<h1>{{ product?.nomproduit }}</h1>
+						<a href="#description">Description détaillée</a>
+						<div class="star-container">
+							<Star />
+							<Star />
+							<Star />
+							<Star />
+							<Star />
+	
+							<p>({{ product.avisNavigation.length }} avis)</p>
+						</div>
+						<p>Coloris disponibles ({{ product.colorationsNavigation.length }})</p>
+	
+						<div class="colorations">
+							<div
+								class="coloration"
+								v-for="coloration in product.colorationsNavigation"
+								v-bind:key="coloration.idcouleur"
+								:style="`--couleur: #${coloration.couleurNavigation.rgbcouleur};`"
+								:data-tooltip="coloration.couleurNavigation.nomcouleur"
+								@click="chosenColoration = coloration"
+							></div>
+						</div>
+						<p v-if="chosenColoration!.prixsolde!=null" class="pourcentage-reduction">-{{ (100-(chosenColoration!.prixsolde)/(chosenColoration!.prixvente)*100).toFixed(0) }} %</p>
+						<div class="prix-container">
+							<p class="prix-solde">{{ chosenColoration!.prixsolde }}€ </p>
+							<p v-if="chosenColoration!.prixvente!=null" class="prix-base">{{ chosenColoration!.prixvente }}€</p>
+						</div>
+						<div class="achat-component">
+							<select id="product-amount-select">
+								<option  v-for="i in product.colorationsNavigation[chosenColoration!.idcouleur].quantitestock" :value="i">{{i}}</option>
+							</select>
+							<button id="buy-button">J'achète</button>
+						</div>
 					</div>
-					<p>Coloris disponibles ({{ product.colorationsNavigation.length }})</p>
-
-					<div class="colorations">
-						<div
-							class="coloration"
-							v-for="coloration in product.colorationsNavigation"
-							v-bind:key="coloration.idcouleur"
-							:style="`--couleur: #${coloration.couleurNavigation.rgbcouleur};`"
-							:data-tooltip="coloration.couleurNavigation.nomcouleur"
-							@click="chosenColoration = coloration"
-						></div>
-					</div>
-					<p v-if="chosenColoration!.prixsolde!=null" class="pourcentage-reduction">-{{ (100-(chosenColoration!.prixsolde)/(chosenColoration!.prixvente)*100).toFixed(0) }} %</p>
-					<div class="prix-container">
-						<p class="prix-solde">{{ chosenColoration!.prixsolde }}€ </p>
-						<p v-if="chosenColoration!.prixvente!=null" class="prix-base">{{ chosenColoration!.prixvente }}€</p>
-
-					</div>
+					
 				</div>
 
 			</div>
@@ -70,6 +79,55 @@ watchEffect(() => {
 	</main>
 </template>
 <style lang="scss">
+.achat-component{
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-top:20px;
+}
+#product-amount-select{
+	background-color:white;
+	border: 1px solid black;
+	border-radius: 5px;
+	height: 70px;
+	font-size: 20px;
+	box-sizing: border-box;
+	width:70px;
+	text-align: center;
+	margin-right: 10%;
+}
+#buy-button{
+	height: 70px;
+	width:210px;
+	box-sizing: border-box;
+	background-color: black;
+	color:white;
+	border: 1px solid black;
+	border-radius: 5px;
+
+}
+.illustrations-card{
+	display:flex;
+}
+.images-card{
+	width:100%;
+}
+.images-card img{
+	width:100%;
+}
+.information-card{
+	position:relative;
+	margin-left:20px;
+
+}
+.scroll-element{
+	width:100%;
+	align-items: center;
+	justify-content: center;
+    top: 0;
+    position:sticky;
+	height: min-content;
+}
 .prix-container{
 	display:flex;
 }
