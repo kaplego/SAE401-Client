2aacf2f9f85773608c87d39453f38ba903e74d2d
Ewan-Ponchaud <ewan.ponchaud@etu.univ-smb.fr>
2025-03-26 09:38:27 +0100

    La searchbar fonctionne mais elle marche pas si on l'utilise une 2ème fois sans reload

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,16 +1,29 @@
 <script setup lang="ts">
 import API from '@/assets/ts/api';
+import { ref, watchEffect } from 'vue';
+import CardProduit from '@/components/CardProduit.vue';
+import { useRouter } from 'vue-router';
 
+const list = ref<Produit[] | null>(null);
+watchEffect(() => {
+	API.products.search(useRouter().currentRoute.value.query.q as string).then((p) => {
+		list.value = p;
+	});
+});
 
-
+console.log(list);
 </script>
 
 <template>
-	<div>
-		sffsdfldsqjfsml
-	</div>
+<div v-if="list !== null" class="grille-produits">
+			<CardProduit v-for="produit in list" v-bind:key="produit.idproduit" :produit="produit" />
+		</div>
+		<template v-else>
+			<div class="loading loading-lg">
+				<div class="loading-spinner"></div>
+			</div>
+		</template>
 </template>
-
 
 <style scoped>
 
