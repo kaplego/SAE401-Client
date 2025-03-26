5328e6f08647140f24a146fa5af93b6141420af4
Ewan-Ponchaud <ewan.ponchaud@etu.univ-smb.fr>
2025-03-26 10:12:10 +0100

    C'est bon pour la searchbar et la page de search

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -5,8 +5,10 @@ import CardProduit from '@/components/CardProduit.vue';
 import { useRouter } from 'vue-router';
 
 const list = ref<Produit[] | null>(null);
+const router = useRouter();
+
 watchEffect(() => {
-	API.products.search(useRouter().currentRoute.value.query.q as string).then((p) => {
+	API.products.search(router.currentRoute.value.query.q as string).then((p) => {
 		list.value = p;
 	});
 });
@@ -15,7 +17,9 @@ console.log(list);
 </script>
 
 <template>
-<div v-if="list !== null" class="grille-produits">
+	<main class="container">
+		<h1>{{ list?.length }} {{ list?.length == 1 ? "Résultat" : "Résultats" }} de recherche pour : {{ router.currentRoute.value.query.q }}</h1>
+		<div v-if="list !== null" class="grille-produits">
 			<CardProduit v-for="produit in list" v-bind:key="produit.idproduit" :produit="produit" />
 		</div>
 		<template v-else>
@@ -23,8 +27,13 @@ console.log(list);
 				<div class="loading-spinner"></div>
 			</div>
 		</template>
+	</main>
 </template>
 
 <style scoped>
-
+.grille-produits {
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	gap: 1rem;
+}
 </style>
