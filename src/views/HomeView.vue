fe907b1ae86fc965f6dfc71e263e2ff9bf991d04
GONTARDLukas <lukas.gontard@etu.univ-smb.fr>
2025-04-08 11:45:30 +0200

    page d'accueil

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,33 @@
 <script setup lang="ts"></script>
 
 <template>
-	<main class="home container">
-		<h1>Miliboo</h1>
-	</main>
+	<div class="header">
+		<h1>Miliboo, les meubles faciles à vivre.</h1>
+		<img src="/public/img/decoration/header.jpg"/>
+
+	</div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.header{
+	position: relative;
+}
+.header img{
+	position: absolute;
+	filter:brightness(60%);
+	width: 100%;
+	object-fit: cover;
+	top:0;
+	left:0;
+}
+.header h1{
+	display: flex;
+    left: 50%;
+	margin-top:200px;
+    transform: translate(-50%, -50%);
+	position: absolute;
+	z-index: 100;
+	font-weight: bold;
+	color:white;
+}
+</style>
